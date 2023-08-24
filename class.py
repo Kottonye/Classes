c1ab9ea2a2ae7440aa383db4d474d29be3766252
Kotton <kauafersantos@icloud.com>
2023-08-23 23:20:06 -0300

    first commit

---
+++ class.py
@@ -0,0 +1,15 @@
+class hero:
+    nome = input("Qual é o seu nome? ")
+    idade = input("Qual a sua idade? ")
+    tipo = input("Qual é a sua classe? ")
+
+if hero.tipo == "guerreiro":
+    print("guerreio atacou usando espada")
+elif hero.tipo == "mago":
+    print("mago usou magia")
+elif hero.tipo == "monge":
+    print("monge usou artes marciais")
+elif hero.tipo == "ninja":
+    print("ninja usou shuriken")
+
+    
